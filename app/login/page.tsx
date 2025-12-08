017173e97537ba9238fd4901a4035008778b9112
Hnzsama <hnzsama@gmail.com>
2025-12-08 23:04:35 +0700

    testing connection to database

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,8 @@
 import { LoginForm } from "@/components/login-form"
 import prisma from "@/lib/prisma"
 
-export default function LoginPage() {
-  const user = prisma.user.findFirst()
+export default async function LoginPage() {
+  const user =  await prisma.user.findFirst()
   console.log(user)
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
