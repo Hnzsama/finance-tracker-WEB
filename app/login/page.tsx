afeb16ad50daa34c23c9a40926b63a78ff880f9d
Hnzsama <hnzsama@gmail.com>
2025-12-09 13:04:44 +0700

    update metadata and logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,6 @@ export default async function LoginPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <p>{user?.name || user?.email}</p>
         <LoginForm />
       </div>
     </div>
